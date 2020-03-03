3bd116956301f78b41475b775fe2d4e5f004af1c
Noah Stapp <noah@noahstapp.com>
2020-03-03 11:34:31 -0800

    Improved readability

--- src/main/scala/KNN.scala
+++ src/main/scala/KNN.scala
@@ -9,19 +9,31 @@ case class Data(sepLen : Double, sepWid : Double, petLen : Double, petWid : Doub
 case class Category(category : String)
 
 class KNN(var neighbors: Int) extends Serializable {
+  // Fit a K-Nearest-Neighbors model with Spark RDD's
   def fit(X: RDD[Data], y : RDD[Category], Xtest: RDD[Data]) : Unit = {
     Xtest.cartesian(X)
-      .map({case (a, b) => (a, (b.category, distance(a, b)))})
-      .sortBy(_._2._2)
-      .groupByKey()
-      .mapValues(v => v.take(neighbors))
+      .map({case (a, b) => (a, (b.category, distance(a, b)))}) // Get each row's distance to every other row
+      .sortBy({case (_, b) => b._2}) // Sort by lowest distance first
+      .groupByKey() // Group by row
+      .mapValues(v => v.take(neighbors)) // Get the N nearest neighbor categories
+      .map({case (a, b) => (a, get_category(b.toList))}) // Get the most likely category for each row
       .collect()
-      .map({case (x,y) => (x, y.map(row => row._1).groupBy(identity).mapValues(_.size).maxBy(_._2)._1)})
       .foreach(println)
   }
+
 //  def predict(Xtest: RDD[Data]): Array[Double] = {
 //    // Predict X test
 //  }
+
+  // Gets the most likely category from a list of category-distance pairs
+  def get_category(distances : List[(String, Double)]) : String = {
+    distances.map({case (category, _) => category}) // Get the category of each category-distance pair
+      .groupBy(identity) // Get all possible categories for each row
+      .mapValues(_.size) // Get the frequency of each possible category
+      .maxBy(_._2)._1 // Get the category that is most frequent for this row
+  }
+
+  // Calculate the euclidean distance between two observations
   def distance(a : Data, b : Data) : Double = {
     var dist = 0.0;
     dist += Math.pow(a.sepLen - b.sepLen, 2.0)
