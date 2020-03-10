7d7f503a4e96d964dcd14e7d628abc2ea0be27fb
Noah Stapp <noah@noahstapp.com>
2020-03-10 11:48:44 -0700

    Log updated

--- src/main/scala/KNN.scala
+++ src/main/scala/KNN.scala
@@ -1,3 +1,4 @@
+package KNN;
 import org.apache.spark.SparkContext._
 
 import scala.io._
@@ -10,8 +11,6 @@ import scala.collection.mutable.ListBuffer
 
 
 case class Data(idx: Long, name: String, totalFunding : Double, rounds : Double, seed : Double, venture : Double, roundA: Double, roundB: Double)
-case class DataEncode(idx: Long, totalFunding : Int, rounds : Int, seed : Int, venture : Int, roundA: Int, roundB: Int,
-                      crowdFunding: Int, angel: Int, privateEquity: Int)
 case class Classification(idx: Long, status : String) {
   override def toString : String = status
 }
@@ -104,31 +103,32 @@ class normalizer() {
       (rec.roundA - minA) / (maxA - minA),
       (rec.roundB - minB) / (maxB - minB)))
   }
-
-
 }
 
 object KNN {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
+    val LOGGER = Logger.getLogger(KNN.getClass)
 
-    val conf = new SparkConf().setAppName("KNN").setMaster("local[4]")
+    val conf = new SparkConf().setAppName("KNN")
+    //      .setMaster("local[4]")
     val sc = new SparkContext(conf)
 
-    val data = sc.textFile("./data/investments.csv")
+    val data = sc.textFile("/user/nstapp/input/investments.csv")
       .map(_.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)")).zipWithIndex()
-      .map({case(line, idx) => Data(idx, line(1), getTotalFunding(line(5).trim()), line(11).toDouble, line(18).toDouble,
-        line(19).toDouble, line(32).toDouble, line(33).toDouble)}).sample(false, 0.01)
+      .map({ case (line, idx) => Data(idx, line(1), getTotalFunding(line(5).trim()), line(11).toDouble, line(18).toDouble,
+        line(19).toDouble, line(32).toDouble, line(33).toDouble)
+      })
 
     val data_idx = data.map(line => (line.idx, line))
 
-    val categories = sc.textFile("./data/investments.csv")
+    val categories = sc.textFile("/user/nstapp/input/investments.csv")
       .map(_.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)")).zipWithIndex()
-      .map({case(line,idx) => Classification(idx, line(6))}).map(line => (line.idx,line))
-      .join(data_idx).map({case (_, pair) => pair._1})
+      .map({ case (line, idx) => Classification(idx, line(6)) }).map(line => (line.idx, line))
+      .join(data_idx).map({ case (_, pair) => pair._1 })
 
-
+    categories.groupBy(_.status).collect().foreach(item => LOGGER.info(item._1 + ", " + item._2.size))
 
     var model = new KNN(5)
 
@@ -147,39 +147,40 @@ object KNN {
 
     val result = model.fit(Xtrain_norm, ytrain, Xtest_norm)
 
-    result.foreach(println)
+    result.foreach(LOGGER.info)
 
     // accuracy
-    println(accuracy(result, ytest.collect()))
+    LOGGER.info(accuracy(result, ytest.collect()))
 
     // precision
-    println(precision(result, ytest.collect()))
+    LOGGER.info(precision(result, ytest.collect()))
 
     // recall
-    println(recall(result, ytest.collect()))
+    LOGGER.info(recall(result, ytest.collect()))
 
     //f1
-    println(f1_score(result, ytest.collect()))
+    LOGGER.info(f1_score(result, ytest.collect()))
 
     //cross_validate
-    val losses = List("recall","precision","f1")
-    for (loss <- losses){
-      println(loss)
+    val losses = List("recall", "precision", "f1")
+    for (loss <- losses) {
+      LOGGER.info(loss)
       for (neighbors <- 1 to 10) {
-        println(neighbors)
+        LOGGER.info(neighbors)
         model = new KNN(neighbors)
-        println(cross_validate(model, data, categories, 5, loss))
+        LOGGER.info(cross_validate(model, data, categories, 5, loss))
       }
     }
 
   }
+
   def extractDouble(x: Any): Option[Double] = x match {
     case n: java.lang.Number => Some(n.doubleValue())
     case _ => None
   }
 
   // Get the total funding from a weirdly formatted string
-  def getTotalFunding(funding: String) : Double = {
+  def getTotalFunding(funding: String): Double = {
     if (funding == "-") {
       return 0
     }
@@ -192,7 +193,7 @@ object KNN {
   (RDD[Data], RDD[Classification], RDD[Data], RDD[Classification]) = {
     val Xtrain = X.sample(false, frac);
     val Xtrain_idx = Xtrain.map(row => (row.idx, row))
-    val ytrain = y.map(row => (row.idx, row)).join(Xtrain_idx).map({case (_, pair) => pair._1})
+    val ytrain = y.map(row => (row.idx, row)).join(Xtrain_idx).map({ case (_, pair) => pair._1 })
     val Xtest = X.subtract(Xtrain)
     val ytest = y.subtract(ytrain)
 
@@ -205,17 +206,17 @@ object KNN {
     val y_idx = y.map(line => (line.idx, line))
 
     // Get Partition size for eah fold
-    val partition_size = X.count()/folds
+    val partition_size = X.count() / folds
     val losses = ListBuffer[(String, Double)]()
 
     // For each fold compute the losses for each class and add to losses list
-    for (i <- 0 until folds){
+    for (i <- 0 until folds) {
       // In this case I make the fold data the test data and the rest the training data
-      var X_fold_data = X_idx.filter({case(idx, line) => ((0 + partition_size * i) <= idx) && (idx <= (0 + partition_size * (i + 1)))}).map(tuple => tuple._2)
-      var X_non_fold_data = X_idx.filter({case(idx, line) => ((0 + partition_size * i) > idx) || (idx > (0 + partition_size * (i + 1)))}).map(tuple => tuple._2)
+      var X_fold_data = X_idx.filter({ case (idx, line) => ((0 + partition_size * i) <= idx) && (idx <= (0 + partition_size * (i + 1))) }).map(tuple => tuple._2)
+      var X_non_fold_data = X_idx.filter({ case (idx, line) => ((0 + partition_size * i) > idx) || (idx > (0 + partition_size * (i + 1))) }).map(tuple => tuple._2)
 
-      var y_fold_data = y_idx.filter({case(idx, line) => ((0 + partition_size * i) <= idx) && (idx <= (0 + partition_size * (i + 1)))}).map(tuple => tuple._2)
-      var y_non_fold_data = y_idx.filter({case(idx, line) => ((0 + partition_size * i) > idx) || (idx > (0 + partition_size * (i + 1)))}).map(tuple => tuple._2)
+      var y_fold_data = y_idx.filter({ case (idx, line) => ((0 + partition_size * i) <= idx) && (idx <= (0 + partition_size * (i + 1))) }).map(tuple => tuple._2)
+      var y_non_fold_data = y_idx.filter({ case (idx, line) => ((0 + partition_size * i) > idx) || (idx > (0 + partition_size * (i + 1))) }).map(tuple => tuple._2)
 
       //ACCURACY IS RETURNING ANY FOR SOME REAONON >:-(
       /*
@@ -232,59 +233,61 @@ object KNN {
       normalizer.fit(X_non_fold_data)
       var X_non_fold_data_norm = normalizer.normalize(X_non_fold_data)
       var X_fold_data_norm = normalizer.normalize(X_fold_data)
-      if (loss == "recall"){
+      if (loss == "recall") {
         var result = model.fit(X_non_fold_data_norm, y_non_fold_data, X_fold_data_norm)
         var loss_val = recall(result, y_fold_data.collect())
         // Make the map into a Sequence of tuples (Class, Loss)
-        var loss_holder = loss_val.map(e => (e._1,e._2))
-        loss_val.map(e => (e._1,e._2)).foreach(e => losses += e)
+        var loss_holder = loss_val.map(e => (e._1, e._2))
+        loss_val.map(e => (e._1, e._2)).foreach(e => losses += e)
       }
-      else if (loss == "precision"){
+      else if (loss == "precision") {
         var result = model.fit(X_non_fold_data, y_non_fold_data, X_fold_data)
         // Make the map into a Sequence of tuples (Class, Loss)
         var loss_val = precision(result, y_fold_data.collect())
-        loss_val.map(e => (e._1,e._2)).foreach(e => losses += e)
+        loss_val.map(e => (e._1, e._2)).foreach(e => losses += e)
       }
-      else if (loss == "f1"){
+      else if (loss == "f1") {
         var result = model.fit(X_non_fold_data, y_non_fold_data, X_fold_data)
         // Make the map into a Sequence of tuples (Class, Loss)
         var loss_val = f1_score(result, y_fold_data.collect())
-        loss_val.map(e => (e._1,e._2)).foreach(e => losses += e)
+        loss_val.map(e => (e._1, e._2)).foreach(e => losses += e)
       }
     }
     // For each class return the mean from all the folds losses
     return losses.groupBy(tuple => tuple._1).mapValues(map_val => map_val.map(value => value._2)).mapValues(loss_list => loss_list.foldLeft(0.0)(_ + _) / loss_list.length)
   }
 
-  def accuracy(result : Array[(Data, String)], ytest : Array[Classification]) : Double = {
-    val ypred_tuple = result.map({case( data, pred) => (data.idx, pred)})
+  def accuracy(result: Array[(Data, String)], ytest: Array[Classification]): Double = {
+    val ypred_tuple = result.map({ case (data, pred) => (data.idx, pred) })
     val ytest_tuple = ytest.map(line => (line.idx, line.status))
     (ypred_tuple ++ ytest_tuple)
       .groupBy(_._1)
-      .values.foreach(x=>println(x.mkString(" ")))
+      .values.foreach(x => println(x.mkString(" ")))
     val ypred_ytest = (ypred_tuple ++ ytest_tuple)
       .groupBy(_._1)
       .values
       .map(tuple_pair => (tuple_pair(0), tuple_pair(1)))
 
     val correct_classifications = ypred_ytest.count({
-      case(pred, actual) => pred._2 == actual._2
+      case (pred, actual) => pred._2 == actual._2
     })
 
     val total_classifications = result.length
 
     var accuracy = correct_classifications * 1.0 / total_classifications
-    if (accuracy.isNaN){accuracy = 0}
+    if (accuracy.isNaN) {
+      accuracy = 0
+    }
 
     accuracy
   }
 
-  def precision(result : Array[(Data, String)], ytest : Array[Classification]) : scala.collection.mutable.Map[String, Double] = {
+  def precision(result: Array[(Data, String)], ytest: Array[Classification]): scala.collection.mutable.Map[String, Double] = {
     val categories = ytest.map(pred => pred.status).distinct
     val precision_map = scala.collection.mutable.Map[String, Double]()
     val category_strings = categories.map(_.toString).distinct
 
-    val ypred_tuple = result.map({case( data, pred) => (data.idx, pred)})
+    val ypred_tuple = result.map({ case (data, pred) => (data.idx, pred) })
     val ytest_tuple = ytest.map(line => (line.idx, line.status))
     val ypred_ytest = (ypred_tuple ++ ytest_tuple)
       .groupBy(_._1)
@@ -304,7 +307,9 @@ object KNN {
 
 
       var precision = true_positives.size * 1.0 / filtered.size
-      if (precision.isNaN){precision = 0}
+      if (precision.isNaN) {
+        precision = 0
+      }
 
       precision_map += (cat -> precision)
     }
@@ -312,12 +317,12 @@ object KNN {
     precision_map
   }
 
-  def recall(result : Array[(Data, String)], ytest : Array[Classification]) : scala.collection.mutable.Map[String, Double] = {
+  def recall(result: Array[(Data, String)], ytest: Array[Classification]): scala.collection.mutable.Map[String, Double] = {
     val categories = ytest.map(pred => pred.status).distinct
     val recall_map = scala.collection.mutable.Map[String, Double]()
     val category_strings = categories.map(_.toString).distinct
 
-    val ypred_tuple = result.map({case( data, pred) => (data.idx, pred)})
+    val ypred_tuple = result.map({ case (data, pred) => (data.idx, pred) })
     val ytest_tuple = ytest.map(line => (line.idx, line.status))
     val ypred_ytest = (ypred_tuple ++ ytest_tuple).
       groupBy(_._1)
@@ -338,21 +343,23 @@ object KNN {
 
       var recall_val = true_positives.size * 1.0 / filtered.size
 
-      if (recall_val.isNaN){recall_val = 0}
+      if (recall_val.isNaN) {
+        recall_val = 0
+      }
 
       recall_map += (cat -> recall_val)
     }
     recall_map
   }
 
-  def f1_score(result : Array[(Data, String)], ytest : Array[Classification]) : scala.collection.mutable.Map[String, Double] = {
+  def f1_score(result: Array[(Data, String)], ytest: Array[Classification]): scala.collection.mutable.Map[String, Double] = {
     val precisionScore = precision(result, ytest)
     val recallScore = recall(result, ytest)
     val categories = ytest.map(pred => pred.status).distinct
     val category_strings = categories.map(_.toString).distinct
     val f1_map = scala.collection.mutable.Map[String, Double]()
     for (cat <- category_strings) {
-      f1_map += (cat -> (2*precisionScore(cat)*recallScore(cat))/(precisionScore(cat)+recallScore(cat)))
+      f1_map += (cat -> (2 * precisionScore(cat) * recallScore(cat)) / (precisionScore(cat) + recallScore(cat)))
     }
 
     return f1_map
